c5677373684b3612d4aec1b9a86a610a7b64097d
takahashi-KZ <ra11.bo20.ok.ey@gmail.com>
2020-10-26 02:43:33 +0900

    コミット

--- src/test.java
+++ src/test.java
@@ -3,5 +3,6 @@ public class test {
         System.out.println("Hello World");
         System.out.println("git test");
         System.out.println("git test2");
+        System.out.println("git test3");
     }
 }
