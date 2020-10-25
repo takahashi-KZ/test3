47d1101b2a4b37ed45b2e645a4aa8215b87342f7
takahashi-KZ <ra11.bo20.ok.ey@gmail.com>
2020-10-26 01:38:28 +0900

    first Commit.

--- src/test.java
+++ src/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("git test");
+        System.out.println("git test2");
     }
 }
