1e7e0b3243428f92d195fec659335fa8acd47058
takahashi-KZ <ra11.bo20.ok.ey@gmail.com>
2020-10-27 01:27:21 +0900

    test7

--- src/test.java
+++ src/test.java
@@ -7,5 +7,6 @@ public class test {
         System.out.println("git test4");
         System.out.println("git test5");
         System.out.println("git test6");
+        System.out.println("git test7");
     }
 }
